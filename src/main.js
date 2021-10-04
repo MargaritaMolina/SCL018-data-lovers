4c26d875640598bcd8d79bb2af646b8867fb52af
Margarita Molina <molmaragrita@gmail.com>
2021-10-04 20:06:55 -0300

    avance 04-10-2021

--- src/main.js
+++ src/main.js
@@ -1,42 +1,46 @@
 //import { example } from './data.js';
 // import data from './data/lol/lol.js';
 //import data from './data/pokemon/pokemon.js';
-import data from './data/rickandmorty/rickandmorty.js';
+import data from "./data/rickandmorty/rickandmorty.js";
 
-/*console.log(example, data);*/
+console.log(example, data);
+console.log(, rickandmorty);
 
 let btnEnter = document.querySelector("#btn-enter");
 btnEnter.addEventListener("click", nextPage);
 
 function nextPage() {
-    document.querySelector(".pag1").style.display = "none";
-    document.querySelector(".pag2").style.display = "block";
+  document.querySelector(".pag1").style.display = "none";
+  document.querySelector(".pag2").style.display = "block";
 }
 
 //guardar la data en una variable
 let allData = data.results;
 //funcion para crear las tarjetas de los personales
 function showCharacter(data) {
-    let character = "";
-    for (let i = 0; i < data.length; i++) {
-        character += `<div class="info-card">
+  let character = "";
+  for (let i = 0; i < data.length; i++) {
+    character += `<div class="info-card">
         <div class="info-card-inner">
           <div class="card" id="card">
-            <img src="${data[i].image} class="cardImage">
-            <p class="characterName">${data[i].name}</p>
+            <img src="${data[i].image}" class="cardImage"/>
+          
+          <div class="characterName">
+            <h3 id="charName">${data[i].name}</h3>
+          </div>
           </div>
           <div class="info-back-card">
-            <img src="/imagenes/logo2.png class=" back-card-logo">
-            <p class="characterInfo"> Estado de vida:${data[i].status}</p>
-            <p class="characterInfo"> Especie:${data[i].species}</p>
-            <p class="characterInfo"> Genero:${data[i].gender}</p>
-            <p class="characterInfo"> Origen:${data[i].origin}</p>
-            <p class="characterInfo"> Locacion:${data[i].location}</p>
+            <img src="/imagenes/RickandMorty.png" class=" back-card-logo">
+            <p class="characterInfo"> Estado de vida: ${data[i].status}</p>
+            <p class="characterInfo"> Especie: ${data[i].species}</p>
+            <p class="characterInfo"> Genero: ${data[i].gender}</p>
+            <p class="characterInfo"> Origen: ${data[i].origin.name}</p>
+            <p class="characterInfo"> Locacion: ${data[i].location.name}</p>
           </div>
         </div>
-      </div>`
-      }
-      return character;
-    }
-    
+      </div>`;
+  }
+  return character;
+}
+
 document.getElementById("characterList").innerHTML = showCharacter(allData);
